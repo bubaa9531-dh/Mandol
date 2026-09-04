42420136b89d48f6fc57d91ed85944f717ef77ba
bubaa9531-dh <bubaa9531@gmail.com>
2026-09-04 21:38:05 +0800

    test(aml): add path bootstrap for AML contract tests

---
+++ tests/conftest.py
@@ -0,0 +1,7 @@
+"""Make the in-repo src/ layout importable when running tests from a checkout."""
+import os
+import sys
+
+_SRC = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
+if _SRC not in sys.path:
+    sys.path.insert(0, _SRC)

40ecc3b2163b85cceb6c9232b36a8b8258d17609
bubaa9531-dh <bubaa9531@gmail.com>
2026-09-04 21:40:34 +0800

    feat(aml): add src/mandol_aml/version.py

---
+++ src/mandol_aml/version.py
@@ -0,0 +1,5 @@
+"""Version of the Mandol-AML adapter."""
+
+# Upstream Mandol is 0.1.0; this adapter version is tracked separately so that
+# the frozen evaluation version can be pinned independently of upstream.
+__version__ = "0.1.0-aml.1"
